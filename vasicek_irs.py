6969fd19336774bd6fff7455dc8f5342d76d64c3
Takuya Otani <23203070+totani-finmath@users.noreply.github.com>
2021-06-11 13:06:35 -0400

    zbo calculation is updated.

--- vasicek_irs.py
+++ vasicek_irs.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import normal
+from scipy.stats import norm
 import importlib
 from copy import deepcopy as copy
 from tqdm import tqdm
@@ -16,6 +17,43 @@ def time_grids(t1,t2,freq=4):
     num_grid = int(t2 * freq) + 1
     ts = np.linspace(t1, t1+t2, num=num_grid)
     return ts
+
+def time_grids_from_s(s, ts):
+    ts_s = []
+    is_1st = True
+    for i in range(1, len(ts)):
+        # if there is fixing CF
+        if ts[i]-s>0.0 and is_1st:
+            ts_s.append(ts[i-1]-s)
+            is_1st = False
+        if ts[i]-s>0.0:
+            ts_s.append(ts[i]-s)
+    return ts_s
+
+# bachelier model
+def swaption_bachelier(S,K,T,sigma,omega):
+    d1 = (S-K)/sigma/np.sqrt(T)
+    return omega*(S-K)*norm.cdf(omega*d1) + sigma*np.sqrt(T)*norm.pdf(omega*d1)
+
+# black model
+def swaption_black(S,K,T,sigma,omega):
+    d1 = (np.log(S/K) + 0.5*sigma**2*T)/sigma/np.sqrt(T)
+    d2 = (np.log(S/K) - 0.5*sigma**2*T)/sigma/np.sqrt(T)
+    return omega * (S*norm.cdf(omega*d1) - K*norm.cdf(omega*d2))
+
+# annuity
+def annuity(dynamics, s, ts_s):
+    tau = [t2 - t1 for t1, t2 in zip(ts_s[:-1], ts_s[1:])]
+    ann = np.sum([tau[i] * dynamics.get_zcb(s, s+ts_s[i+1]) for i in range(len(tau))], axis=0)
+    return ann
+
+# swap atm rate
+def swap_atm_ts(dynamics, s, ts):
+    ts_s = time_grids_from_s(s, ts)
+    denom = annuity(dynamics, s, ts_s)
+    numer = dynamics.get_zcb(s, s+ts_s[0]) - dynamics.get_zcb(s, s+ts_s[-1])
+    return numer/denom
+
 
 # %% --------------------------------------------------
 # Vasicek class definitions
@@ -50,15 +88,22 @@ class Vasicek:
             hist_rs[i+1, :] = self.__proceed_time(dt)
         self.hist_rs = hist_rs
 
+    def B(self, t1, t2):
+        return 1/self.theta*(1 - np.exp(-self.theta*(t2-t1)))
+
+    def A(self, t1, t2):
+        trm1 = (self.mu - self.sigma**2/self.theta**2/2)*(self.B(t1, t2) - (t2 - t1))
+        trm2 = self.sigma**2/self.theta/4*self.B(t1, t2)**2
+        return np.exp(trm1 - trm2)
+
     def get_zcb(self, s, t):
         # corresponding index to time s
         idx = np.argmin(np.abs(self.ts - s))
-        tau = t - s
         # extract parameters
         theta, mu, sigma = self.theta, self.mu, self.sigma
         # coefficients
-        B = 1/theta*(1 - np.exp(-theta*tau))
-        A = np.exp((mu - sigma**2/theta**2/2)*(B - tau) - sigma**2/theta/4*B**2)
+        B = self.B(s,t)
+        A = self.A(s,t)
         # discount bond
         zcb = A.reshape(1, -1)*np.exp(-B.reshape(1, -1)*self.hist_rs[idx,:].reshape(-1, 1))
         return zcb.flatten()
@@ -67,6 +112,14 @@ class Vasicek:
         # corresponding index to time s
         fwd = (self.get_zcb(s,t1)/self.get_zcb(s,t2) - 1.0) / (t2 - t1)
         return fwd.flatten()
+
+    def get_zbo(self, s, t_opt, t_bnd, X, omega=1):
+        # Brigo (3.10): omega = 1 for call
+        sigma_p = self.sigma*np.sqrt((1-np.exp(-2*self.theta*(t_opt-s)))/2/self.theta) * self.B(t_opt, t_bnd)
+        h = np.log(self.get_zcb(s,t_bnd)/self.get_zcb(s,t_opt)/X)/sigma_p + sigma_p/2
+        price = self.get_zcb(s,t_bnd)*norm.cdf(omega*h) - X*self.get_zcb(s,t_opt)*norm.cdf(omega*(h-sigma_p))
+        return omega*price
+
 
 # %% --------------------------------------------------
 # IRS class definitions
@@ -88,21 +141,8 @@ class IRS:
         if np.max(ts_float)<=s or np.max(ts_fixed)<=s:
             return np.zeros(dynamics.num_mc)
         # new grids
-        ts_fixed_s, ts_float_s = [], []
-        is_fixed1st = True
-        for i in range(len(ts_fixed)):
-            if ts_fixed[i]-s>0.0 and is_fixed1st:
-                ts_fixed_s.append(ts_fixed[i-1]-s)
-                is_fixed1st = False
-            if ts_fixed[i]-s>0.0:
-                ts_fixed_s.append(ts_fixed[i]-s)
-        is_float1st = True
-        for i in range(len(ts_float)):
-            if ts_float[i]-s>0.0 and is_float1st:
-                ts_float_s.append(ts_float[i-1]-s)
-                is_float1st = False
-            if ts_float[i]-s>0.0:
-                ts_float_s.append(ts_float[i]-s)
+        ts_fixed_s = time_grids_from_s(s, ts_fixed)
+        ts_float_s = time_grids_from_s(s, ts_float)
         tau_fixed_s = [ts_fixed_s[i+1] - ts_fixed_s[i] for i in range(len(ts_fixed_s)-1)]
         tau_float_s = [ts_float_s[i+1] - ts_float_s[i] for i in range(len(ts_float_s)-1)]
         # fixed leg pv
@@ -126,13 +166,13 @@ class IRS:
 # test
 # -----------------------------------------------------
 # simulation setting
-num_sim = 40000
-dt = 0.01
+num_sim = 100
+dt = 0.025
 horizon = 10.0
+
 # build instances
 dynamics = Vasicek(r0=0.01, theta=0.85, mu=0.04, sigma=0.08)
 contract = IRS(X=0.03, tenor=7.0)
-
 # simulation
 dynamics.monte_carlo_simlation(dt=0.01, num_mc=num_sim, horizon=horizon)
 ts_sim = np.linspace(0.0, 8.0, num=500)
@@ -143,6 +183,15 @@ plt.plot(ts_sim, np.minimum(price,0.0).mean(axis=1), label='ENE')
 plt.plot(ts_sim, price.mean(axis=1), label='EE')
 plt.show()
 
+
+# %% --------------------------------------------------
+# test
+# -----------------------------------------------------
+s = 1.0
+t_opt = 2.0
+t_bnd = 5.0
+test = dynamics.get_zbo(s, t_opt, t_bnd, dynamics.get_zcb(t_opt, t_bnd))
+print(test)
 
 
 # %%
