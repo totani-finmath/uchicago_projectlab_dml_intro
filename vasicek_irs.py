9b5d5a3ff8175b624d3f51aba15310b1d31f2ed2
Takuya Otani <23203070+totani-finmath@users.noreply.github.com>
2021-06-11 07:59:15 -0400

    draft code is updated.

--- vasicek_irs.py
+++ vasicek_irs.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from numpy.random import normal
 import importlib
 from copy import deepcopy as copy
-
+from tqdm import tqdm
 
 # %% --------------------------------------------------
 # Helper function
@@ -23,48 +23,48 @@ def time_grids(t1,t2,freq=4):
 # Dynamics class
 class Vasicek:
 
-    def __init__(self, r0=0.04, theta=0.30, mu=0.05, 
-                 sigma=0.08, num_mc=1000, seed=1234):
+    def __init__(self, r0=0.01, theta=0.85, mu=0.04, sigma=0.05):
         # set up
         self.r0 = r0
         self.theta = theta
         self.mu = mu
         self.sigma = sigma
-        self.num_mc = num_mc
-        # initialize state
-        self.rs = np.ones(num_mc) * r0
-        self.hist_rs = None
-        np.random.seed(seed)
 
-    def proceed_time(self, dt):
+    def __proceed_time(self, dt):
         self.rs = self.rs + self.theta*(self.mu - self.rs)*dt \
                   + self.sigma*normal(size=self.num_mc) * np.sqrt(dt)
         return self.rs
 
-    def monte_carlo_simlation(self, dt=0.01, horizon=10.0):
+    def monte_carlo_simlation(self, dt=0.01, num_mc=1000, horizon=10.0, seed=1234):
+        # initialize state
+        self.num_mc = num_mc
+        self.rs = np.ones(num_mc) * self.r0
+        self.hist_rs = None
+        np.random.seed(seed)
         num_grids = int(horizon/dt) + 1
         self.ts = np.linspace(0.0, horizon, num=num_grids)
+        # simulation
         hist_rs = np.zeros((num_grids, self.num_mc), np.float)
         hist_rs[0, :] = self.rs
-        for i in range(num_grids-1):
-            hist_rs[i+1, :] = self.proceed_time(dt)
+        for i in tqdm(range(num_grids-1)):
+            hist_rs[i+1, :] = self.__proceed_time(dt)
         self.hist_rs = hist_rs
 
     def get_zcb(self, s, t):
         # corresponding index to time s
         idx = np.argmin(np.abs(self.ts - s))
+        tau = t - s
         # extract parameters
         theta, mu, sigma = self.theta, self.mu, self.sigma
         # coefficients
-        B = 1 / theta * (1 - np.exp(-theta * (t - s)))
-        A = np.exp((mu - sigma**2 / theta**2 / 2) * (B - t + s) - sigma**2 / theta / 4 * B**2)
+        B = 1/theta*(1 - np.exp(-theta*tau))
+        A = np.exp((mu - sigma**2/theta**2/2)*(B - tau) - sigma**2/theta/4*B**2)
         # discount bond
-        zcb = A.reshape(1, -1) * np.exp(-B.reshape(1, -1) * self.hist_rs[idx, :].reshape(-1, 1))
+        zcb = A.reshape(1, -1)*np.exp(-B.reshape(1, -1)*self.hist_rs[idx,:].reshape(-1, 1))
         return zcb.flatten()
 
     def get_fwd(self, s, t1, t2):
         # corresponding index to time s
-        idx = np.argmin(np.abs(self.ts - s))
         fwd = (self.get_zcb(s,t1)/self.get_zcb(s,t2) - 1.0) / (t2 - t1)
         return fwd.flatten()
 
@@ -73,7 +73,7 @@ class Vasicek:
 # -----------------------------------------------------
 class IRS:
 
-    def __init__(self, tenor=7.0, X=0.03, side=1):
+    def __init__(self, tenor=7.0, X=0.05, side=1):
         self.T = tenor  # contract lifespan
         self.X = X      # fixed interest rate
         self.side = side  # 1 for payer, -1 for receiver
@@ -84,6 +84,9 @@ class IRS:
         # copy params
         ts_fixed = self.ts_fixed
         ts_float = self.ts_float
+        # error handle
+        if np.max(ts_float)<=s or np.max(ts_fixed)<=s:
+            return np.zeros(dynamics.num_mc)
         # new grids
         ts_fixed_s, ts_float_s = [], []
         is_fixed1st = True
@@ -103,29 +106,43 @@ class IRS:
         tau_fixed_s = [ts_fixed_s[i+1] - ts_fixed_s[i] for i in range(len(ts_fixed_s)-1)]
         tau_float_s = [ts_float_s[i+1] - ts_float_s[i] for i in range(len(ts_float_s)-1)]
         # fixed leg pv
-        pv_fixed = np.array([tau_fixed_s[i] * dynamics.get_zcb(s, ts_fixed_s[i+1]) * self.X \
+        s_prev = s + ts_float_s[0]
+        pv_fixed = np.array([tau_fixed_s[i] * dynamics.get_zcb(s, s+ts_fixed_s[i+1]) * self.X \
                                 for i in range(len(tau_fixed_s))]).sum(axis=0).flatten()
         # float leg pv: 1st CF being fixed
-        pv_float = np.array(tau_float_s[0] * dynamics.get_zcb(s, ts_float_s[1]) \
-                            * dynamics.get_fwd(s, ts_float_s[0], ts_float_s[1]))
+        pv_float = np.array(tau_float_s[0] * dynamics.get_zcb(s, s+ts_float_s[1]) \
+                            * dynamics.get_fwd(s_prev, s_prev, s+ts_float_s[1]))
         # float CF after 1st CF
         if len(tau_float_s) > 1: # if still exist floating CF
-            pv_float2 = np.array([tau_float_s[i] * dynamics.get_zcb(s, ts_float_s[i+1]) \
-                                 * dynamics.get_fwd(s, ts_float_s[i], ts_float_s[i+1]) \
-                               for i in range(1,len(tau_float_s))]).sum(axis=0).flatten()
-            pv_float += pv_float
+            pv_float2 = np.array([tau_float_s[i] * dynamics.get_zcb(s, s+ts_float_s[i+1]) \
+                                 * dynamics.get_fwd(s, s+ts_float_s[i], s+ts_float_s[i+1]) \
+                                 for i in range(1,len(tau_float_s))]).sum(axis=0).flatten()
+            pv_float = pv_float + pv_float2
         return self.side * (pv_float - pv_fixed)
+        #return pv_float
+        #return pv_fixed
 
 # %% --------------------------------------------------
 # test
 # -----------------------------------------------------
-dynamics = Vasicek()
-dynamics.monte_carlo_simlation()
-plt.plot(dynamics.hist_rs[:,0])
+# simulation setting
+num_sim = 40000
+dt = 0.01
+horizon = 10.0
+# build instances
+dynamics = Vasicek(r0=0.01, theta=0.85, mu=0.04, sigma=0.08)
+contract = IRS(X=0.03, tenor=7.0)
 
-contract = IRS()
-test = contract.pricing(0.3333, dynamics)
+# simulation
+dynamics.monte_carlo_simlation(dt=0.01, num_mc=num_sim, horizon=horizon)
+ts_sim = np.linspace(0.0, 8.0, num=500)
+price = np.array([contract.pricing(t, dynamics) for t in ts_sim])
+# figure
+plt.plot(ts_sim, np.maximum(price,0.0).mean(axis=1), label='EPE')
+plt.plot(ts_sim, np.minimum(price,0.0).mean(axis=1), label='ENE')
+plt.plot(ts_sim, price.mean(axis=1), label='EE')
+plt.show()
+
+
 
 # %%
-
-
